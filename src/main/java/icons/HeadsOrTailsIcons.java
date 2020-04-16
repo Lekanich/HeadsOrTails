0efeaa0fff1e73742f6447ceef791ca1978067c7
lekanich <lekan1992@gmail.com>
2020-04-16 18:41:09 +0300

    - add plugin icon and coin icon

--- src/main/java/icons/HeadsOrTailsIcons.java
+++ src/main/java/icons/HeadsOrTailsIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class HeadsOrTailsIcons {
-	public static final Icon COIN = IconLoader.getIcon("/icons/task.png");
+	public static final Icon COIN = IconLoader.getIcon("/icons/coin.svg");
 }
