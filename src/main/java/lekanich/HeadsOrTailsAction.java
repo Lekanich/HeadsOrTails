998253325f183b2b92be01c5eeedf860df2335ca
lekanich <lekan1992@gmail.com>
2020-12-03 00:00:20 +0200

    * add icon to notification

--- src/main/java/lekanich/HeadsOrTailsAction.java
+++ src/main/java/lekanich/HeadsOrTailsAction.java
@@ -5,18 +5,19 @@ import com.intellij.notification.NotificationDisplayType;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
+import com.intellij.notification.Notifications.Bus;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.diagnostic.Logger;
+import icons.HeadsOrTailsIcons;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 public class HeadsOrTailsAction extends AnAction {
+    private static final Logger LOGGER = Logger.getInstance(HeadsOrTailsAction.class);
     private static final double EDGE_CONST = 1.0E-9;
     private static final double HALF_CONST = 0.5 + EDGE_CONST / 10;
-    public static final Logger LOGGER = Logger.getInstance(HeadsOrTailsAction.class);
 
     @RequiredArgsConstructor
     public enum Coin {
@@ -54,7 +55,7 @@ public class HeadsOrTailsAction extends AnAction {
     }
 
     private void notify(@NotNull AnActionEvent e, String title, String subTitle, String message) {
-        NotificationGroup group = new NotificationGroup(message, NotificationDisplayType.BALLOON, true);
+        NotificationGroup group = new NotificationGroup(message, NotificationDisplayType.BALLOON, true, null, HeadsOrTailsIcons.COIN);
         Notification notification = group.createNotification(
                 title,
                 subTitle,
@@ -63,7 +64,7 @@ public class HeadsOrTailsAction extends AnAction {
                 new NotificationListener.UrlOpeningListener(false)
         );
 
-        Notifications.Bus.notify(notification, e.getProject());
+        Bus.notify(notification, e.getProject());
     }
 
     @Override
